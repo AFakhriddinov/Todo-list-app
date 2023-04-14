2688541380f14f45d637f979b57120d9a3d74fd5
OsmanFP <osmanwako411@gmail.com>
2023-04-14 19:12:53 +0300

    update add test function

--- src/modules/todolist.test.js
+++ src/modules/todolist.test.js
@@ -3,9 +3,8 @@ jest.mock('./todolist.js');
 const TodoList = require('./todolist.js');
 
 // Add
-
 describe('addTask', () => {
-  it('Add task', () => {
+  it('Add Task', () => {
     const todoList = new TodoList();
     todoList.add('Write code');
     expect(todoList.tasks).toHaveLength(1);
@@ -27,5 +26,41 @@ describe('addTask', () => {
       description: 'Test code',
       completed: false,
     });
+  });
+
+  // a function for editing the task description you need to run firste
+
+  test('should update the task description', () => {
+    // create a new TodoList instance
+    const todoList = new TodoList();
+    // simulate a change event on the textArea element
+    const value = 'Test code';
+    todoList.editTask(0, value);
+    // assert that the task description has been updated correctly
+    expect(todoList.tasks[0].description).toEqual('Test code');
+  });
+
+  // Test  the showCompleted function.
+  test('showCompleted function sets completed property of all tasks to true', () => {
+    const todoList = new TodoList();
+    todoList.add('Write code');
+    todoList.add('Test code');
+    todoList.showCompleted();
+    todoList.tasks.forEach((task) => {
+      expect(task.completed).toBe(true);
+    });
+  });
+
+  // Test the "Clear all completed" function.
+  test('cleanCompleted should remove all completed tasks from the tasks array', () => {
+    const todoList = new TodoList();
+    todoList.add('Task 1');
+    todoList.add('Task 2');
+    todoList.add('Task 3');
+    todoList.tasks[1].completed = true;
+    todoList.cleanCompleted();
+    expect(todoList.tasks.length).toBe(2);
+    expect(todoList.tasks[0].description).toBe('Task 1');
+    expect(todoList.tasks[1].description).toBe('Task 3');
   });
 });
